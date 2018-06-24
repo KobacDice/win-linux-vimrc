427a1283108eaa4dda415aa7cdd405d0619db44c
Kobacdice <sora2973das@yahoo.co.jp>
2018-06-24 20:39:03 +0900

    modified ctags exec script

--- ctag_cmd.sh
+++ ctag_cmd.sh
@@ -1,4 +1,4 @@
-REM #!/bin/sh
- ctags -f $HOME/.ctags --totals -R /code/*
+#!/bin/sh
+#ctags -f $HOME/.ctags --totals -R /code/*
 
 ctags -f %userprofile%\.ctags  --totals -R %userprofile%/ioq3/code/*
